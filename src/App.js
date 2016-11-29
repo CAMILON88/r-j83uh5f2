0d115bd7608416a05b18eafab2fa65c42bbb3378
German Escobar <german.escobarc@gmail.com>
2016-11-29 02:42:19 -0500

    Version mejorada y actualizada

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
+
+
 class App extends Component {
+  constructor() {
+     super()
+     this.state = {
+      keep: 0,
+      words:0,
+      set:false,
+      text: ""
+     }
+   }
+
+    textChange (event) {
+      this.setState({ text: event.target.value })
+      if (this.state.set === false) {
+         this.setState({
+            keep: event.target.value.length,
+            words: this.wordCount(event.target.value),
+         })
+      }
+    }
+    wordCount (text) {
+    	var arr = text.split(" ");
+      return arr.reduce(function(count, item) {
+        if (item.length > 0) { count++; }
+      	return count;
+      }, 0);
+    }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <textarea rows="3" onChange={this.textChange.bind(this)}></textarea>
+        <div className="counter">{this.state.keep}</div>
       </div>
     );
   }
